4e5f5cf501406426a2e1e4dfe435f62194740bee
Bohdan Kret <kretbohdan2002@gmail.com>
2025-07-21 10:09:50 +0300

    stupid update

--- src/task-8.ts
+++ src/task-8.ts
@@ -6,22 +6,13 @@ interface Post {
     body: string,
 }
 
-async function fetchPosts(): Promise<Post[] | null > {
-    try {
-      const response = await axios.get<Post[]>(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      return response.data;
-    } catch (error) {
-      console.error("Error retrieving posts:", error);
-      return null;
-    }
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return response.data;
 }
 
 fetchPosts().then((posts) => {
-  if (posts && posts.length > 0) {
-    console.log(posts[0].title);
-  } else {
-    console.log("Posts not loaded or array is empty");
-  }
+  console.log(posts[0].title);
 });
